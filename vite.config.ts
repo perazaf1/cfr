6e2a92d434fff34cc2223274d8948cc66603eb7a
perazaf1 <pe.razafindrakoto@gmail.com>
2025-04-07 22:42:08 +0300

    changed branch

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",  // Lien pour GitHub Pages
+  base: "/cfr",  // Lien pour GitHub Pages
 })
