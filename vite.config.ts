f7cb8ee8f650178e3900465202b2d61fd7b21776
perazaf1 <pe.razafindrakoto@gmail.com>
2025-04-07 22:33:43 +0300

    changed url

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/crazyfamilyrun",  // Lien pour GitHub Pages
+  base: "/cfr",  // Lien pour GitHub Pages
 })
